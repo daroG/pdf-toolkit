488c4572d3ffb3017332b1c943677f1aa36ab01a
daroG <darek.kce@gmail.com>
2024-10-27 09:09:44 +0100

    Ruff test

--- tests/commands/test_pdf_command.py
+++ tests/commands/test_pdf_command.py
@@ -1,5 +1,5 @@
-
 import pytest
+
 from pdf_toolkit.commands.pdf_command import PdfCommand
 
 
@@ -13,5 +13,5 @@ from pdf_toolkit.commands.pdf_command import PdfCommand
         ('1-10,5-7,8-10, 3, 2, 1', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
     ],
 )
-def test_translate_pages_string(input: str, expected_pages: list[int]):
-    assert PdfCommand.translate_pages_string(input) == expected_pages
+def test_translate_pages_string(input_value: str, expected_pages: list[int]):
+    assert PdfCommand.translate_pages_string(input_value) == expected_pages

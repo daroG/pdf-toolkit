7440318d88cba1b23521d84946412be61c723c36
daroG <darek.kce@gmail.com>
2024-11-04 11:41:16 +0100

    Make file list module more readable

--- pdf_toolkit/gui/file_list.py
+++ pdf_toolkit/gui/file_list.py
@@ -104,38 +104,67 @@ class EditableTreeview(ttk.Treeview):
         self.bind('<Double-Button-1>', self._handle_edit_event)
 
     @staticmethod
-    def translate_column_raw_id(row_id: str) -> int:
-        return int(row_id[1:], base=16)
+    def translate_column_raw_id(raw_id: str) -> int:
+        """
+        Get integer value from TK raw id.
+
+        :param row_id: TK raw id
+        :return: integer id
+        """
+        return int(raw_id[1:], base=16)
 
     def _handle_edit_event(self, event: tk.Event) -> None:
-        rowid, column = self.identify_row_and_column(event.x, event.y)
-        if not self.is_column_editable(column):
+        row_raw_id, column_raw_id = self.identify_row_and_column(event.x, event.y)
+        if not self.is_column_editable(column_raw_id):
             return
 
         # get column position info
-        bbox_values = self.bbox(rowid, column)
+        bbox_values = self.bbox(row_raw_id, column_raw_id)
         if isinstance(bbox_values, str):
             return
         x, y, _, height = bbox_values
         pady = height // 2
 
         # place Entry popup properly
-        values = self.get_all_row_values(rowid)
-        self.entryPopup = RecordEntryPopup(self, rowid, values, self.translate_column_raw_id(column))
+        values = self.get_all_row_values(row_raw_id)
+        self.entryPopup = RecordEntryPopup(
+            self,
+            row_raw_id,
+            values,
+            self.translate_column_raw_id(column_raw_id),
+        )
         self.entryPopup.place(x=x, y=y + pady, anchor=tk.W, relwidth=1)
 
     def identify_row_and_column(self, x: int, y: int) -> tuple[str, str]:
-        rowid = self.identify_row(y)
-        column = self.identify_column(x)
-        return rowid, column
+        """
+        Identify row and column raw idexes under given widget coordinates.
 
-    def get_all_row_values(self, rowid: str) -> tuple[str, ...]:
-        text = self.item(rowid, 'text')
-        values = self.item(rowid, 'value')
+        :param x: x coordinate
+        :param y: y coordinate
+        :return: tuple of row raw id and column raw id
+        """
+        row_raw_id = self.identify_row(y)
+        column_raw_id = self.identify_column(x)
+        return row_raw_id, column_raw_id
+
+    def get_all_row_values(self, row_raw_id: str) -> tuple[str, ...]:
+        """
+        Get all row values.
+
+        :param rowid: raw row id
+        :return: all column by column values from specific row
+        """
+        text = self.item(row_raw_id, 'text')
+        values = self.item(row_raw_id, 'value')
         return (text, *values)
 
     def is_column_editable(self, column_raw_id: str) -> bool:
-        # column_raw_id is #1
+        """
+        Determine if column is editable.
+
+        :param column_raw_id: column raw id
+        :return: whether the column is editable
+        """
         column_id = self.translate_column_raw_id(column_raw_id)
         column_settings = self._columns[self._columns_mapping[column_id]]
         if isinstance(column_settings, str):
@@ -144,7 +173,6 @@ class EditableTreeview(ttk.Treeview):
 
     def _flatten_columns(self, columns: dict[str, Any]) -> list[str]:
         return list(columns.keys())[1:]
-
 
 
 class FileList(EditableTreeview, DraggableTreeview):
@@ -164,6 +192,11 @@ class FileList(EditableTreeview, DraggableTreeview):
         self._files: list[PdfSource] = []
 
     def add_file(self, file: PdfSource) -> None:
+        """
+        Add file to file list.
+
+        :param file: file source
+        """
         index = self._calculate_index(self.selection())
         self.insert('', index, text=file.name, values=(file.pages_count, f'1-{file.pages_count}'))
         self._files.append(file)
